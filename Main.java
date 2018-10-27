dbb0baaaf5b1b5873432146935f6d5775cd74c38
ttmx <tiago.sequeira.teles@gmail.com>
2018-10-27 01:11:14 +0100

    Main theoretically kinda done

--- Main.java
+++ Main.java
@@ -1,6 +1,72 @@
-class Java{
+/**@author Tiago Teles */
+/* Todas as funcoes com main no inicio do nome referem-se ao que acontece antes de haver um jogo iniciado,
+*  as com game no inicio serao entao as ja dentro do jogo.
+*/
+import java.util.Scanner;;
+class Main{
+    private static final String SAIR = "sai";
+    private static final String JOGAR = "joga";
+    private static final String AJUDA = "ajuda";
+    private static final String NOVOJOGO = "novo";
+    private static final String FIM = "fim";
+    
     public static void main(String[] args) {
-        trespacito
+        Scanner scan = new Scanner(System.in);
+        FCTMil milObj = new FCTMil();
+        mainCommands(scan,milObj);
     }
+    //Aqui escolhemos o que fazer com os comandos inseridos na consola quando fora do um jogo
+    public static void mainCommands(Scanner scan,FCTMil milObj){
+        System.out.print("> ");
+        String command = scan.next();
+        switch(command.toLowerCase()){
+            case SAIR:mainLeave(scan, milObj); 
+                break;
+            case AJUDA:mainHelp(scan, milObj);
+                break;
+            case NOVOJOGO:gameStart(scan, milObj);
+                break;
+            default: 
+                System.out.println("Comando inexistente.");
+                mainCommands(scan,milObj);
+                break;
+        }
+    }
+    //Aqui escolhemos o que fazer com os comandos inseridos na consola quando fora do um jogo
+    public static void gameCommands(Scanner scan, FCTMil milObj){
+        System.out.print("FCTMILHOES> ");
+        String command = scan.next();
+        switch(command.toLowerCase()){
+            case FIM:gameEnd(scan,milObj);
+                break;
+            case JOGAR:gamePlay(scan,milObj);
+                break;
+            case AJUDA:gameHelp(scan,milObj);
+                break;
+            default: System.out.println("Comando inexistente.");
+                break;
+        }
+     }
+    //Funcoes de interface
 
+    public static void mainHelp(Scanner scan, FCTMil milObj){
+        System.out.println(NOVOJOGO +" - Novo jogo dando um valor inicial\n"+ SAIR +" - Termina a execucao do programa\n"+ AJUDA +" - Mostra os comandos existentes");
+        mainCommands(scan, milObj);
+    }
+    public static void mainLeave(Scanner scan, FCTMil milObj){
+         
+    }
+    public static void gameEnd(Scanner scan, FCTMil milObj){
+         
+    }
+    public static void gameStart(Scanner scan, FCTMil milObj){
+         
+    }
+    public static void gamePlay(Scanner scan, FCTMil milObj){
+         
+    }
+    public static void gameHelp(Scanner scan, FCTMil milObj){
+        System.out.println(JOGAR + " - Simula uma aposta, dando uma chave\n" + FIM + " - Termina o jogo em curso\n" + AJUDA + " - Mostra os comandos existentes");
+        gameCommands(scan, milObj);
+    }
 }
