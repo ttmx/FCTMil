637b52e0d8325a1e898a77dddc7a52b28356541f
ttmx <tiago.sequeira.teles@gmail.com>
2018-11-16 13:24:57 +0000

    we dun now

--- FCTMil.java
+++ FCTMil.java
@@ -1,28 +1,36 @@
 //import java.util.Arrays;
 class FCTMil{
+
     private int[] winnerArray;
-    private Double prizePool;
+
+    private double prizePool;
+
     private boolean inGame;
     private boolean inMenu;
+
     private IteratorInt starIte;
     private IteratorInt numIte;
+
     private final int STARS;
     private final int NUMBERS;
-    Key keyC;
+
+    private Key keyC;
+
     public FCTMil(){
-    winnerArray = new int[13];
-    inGame = false;
-    inMenu = true;
-    STARS = 2;
-    NUMBERS = 5;
-    prizePool = 0d;
+        winnerArray = new int[13];
+        inGame = false;
+        inMenu = true;
+        STARS = 2;
+        NUMBERS = 5;
+        prizePool = 0.;
     }
+
     /**
      * @param prizePool game prize pool
      */
-    public void newGame(Double prizePool){
+    public void newGame(double prizePool) {
         this.prizePool += prizePool;
-        for(int i=0; i<winnerArray.length; i++){
+        for (int i=0; i<winnerArray.length; i++) {
             winnerArray[i] = 0;
         }
         keyC = new Key();
@@ -30,175 +38,161 @@ class FCTMil{
         numIte = keyC.criaIteratorNumbers();
         inGame = true;
     }
+
     // Betting is directed here
     public int bet(int[] bets){
-        boolean invalid = isValid(bets);
+        boolean invalid = isInvalid(bets);
         int[] currentKey = getKey();
         //System.out.println(Arrays.toString(bets));
         int prizeNum = -1;
-        if (!invalid)
-        prizeNum = prize(currentKey,bets);
-        if (prizeNum>0){
+        if (!invalid) {
+            prizeNum = prize(currentKey,bets);
+        }
+
+        if (prizeNum>0) {
             winnerArray[prizeNum-1]++;
         }
         //System.out.println(Arrays.toString(winnerArray));
         return prizeNum;
     }
-    private boolean isValid(int[] key){
+
+    /**
+     * Tests if the key has duplicates or invalid numbers
+     * @param key Key to be tested
+     * @return returns true if it is invalid and false if its valid
+     */
+    private boolean isInvalid(int[] key){
         boolean invalid = false;
-        for(int i = 0; i<NUMBERS;i++){
-            for(int a = 0; a<NUMBERS;a++){
-                if (i!=a)
-                if(key[i]==key[a]){
-                    a = NUMBERS;
-                    i = NUMBERS;
-                    invalid = true;
+        for(int i = 0; i<NUMBERS; i++) {
+            for(int a = 0; a<NUMBERS; a++) {
+                if (i != a) {
+                    if(key[i] == key[a]) {
+                        a = NUMBERS;
+                        i = NUMBERS;
+                        invalid = true;
+                    }
                 }
             }
         }
-        for(int i = 0; i<NUMBERS;i++){
-            if (key[i]>50 ||key[i]<1)
+        for(int i = 0; i<NUMBERS; i++) {
+            if (key[i] > 50 || key[i] < 1) {
+                invalid = true;
+            }
+        }
+        for(int i = NUMBERS; i < NUMBERS + STARS; i++) {
+            if (key[i] > 12 || key[i] < 1) {
+                invalid = true;
+            }
+        }
+        if(key[key.length - 2] == key[key.length - 1]) {
             invalid = true;
         }
-        for(int i = NUMBERS; i<NUMBERS+STARS;i++){
-            if (key[i]>12 ||key[i]<1)
-            invalid = true;
-        }
-        if(key[key.length-2]==key[key.length-1])
-        invalid = true;
         return invalid;
     }
+
     // Return current key array
-    private int[] getKey(){
+    private int[] getKey() {
         int [] key = new int[7];
-        for(int i = 0;numIte.hasNextInt();i++){
-            key[i]=numIte.nextInt();
+        for(int i = 0; numIte.hasNextInt(); i++) {
+            key[i] = numIte.nextInt();
         }
-        for(int i = 0;starIte.hasNextInt();i++){
-            key[i+NUMBERS]=starIte.nextInt();
+        for(int i = 0; starIte.hasNextInt(); i++){
+            key[i + NUMBERS] = starIte.nextInt();
         }
         numIte.reinitialize();
         starIte.reinitialize();
         return key;
     }
+
     // Takes in an int[] with the correct key and another one with the placed bet and outputsan int with the prize level, or 0 if theres is no prize
     private int prize(int[] key,int[] bet){
         int hitNums = 0;
         int hitStars = 0;
-        for(int i = 0;i<NUMBERS;i++){
-            for(int a = 0;a<NUMBERS;a++){
+        //hitNums = the number of hit numbers
+        for(int i = 0; i < NUMBERS; i++) {
+            for(int a = 0; a < NUMBERS; a++) {
                 if(key[a] == bet[i]){
                     hitNums++;
                 }
             }
         }
-        for(int i = NUMBERS;i<NUMBERS + STARS;i++){
-            for(int a = NUMBERS;a<NUMBERS + STARS;a++){
+        //hitStars = the number of hit stars
+        for(int i = NUMBERS; i < NUMBERS + STARS; i++){
+            for(int a = NUMBERS; a < NUMBERS + STARS; a++){
                 if(key[a] == bet[i]){
                     hitStars++;
                 }
             }
         }
         // Since hitNums and hitStars are both lower than 10 I can use them as a sort of index for the switch statement
-        int prizeSelector = 10*hitNums + hitStars;
-        switch(prizeSelector){
-            case 52:
-                prizeSelector = 1;
+        int prizeSelector = 10 * hitNums + hitStars;
+        int level = 0;
+        int[] prizesArray = {52, 51, 50, 42, 41, 32, 40, 22, 31, 30, 12, 21, 20};
+        for(int i = 0; i < prizesArray.length; i++) {
+            if(prizesArray[i] == prizeSelector) {
+                level = i + 1;
                 break;
-            case 51:
-                prizeSelector = 2;
-                break;
-            case 50:
-                prizeSelector = 3;
-                break;
-            case 42:
-                prizeSelector = 4;
-                break;
-            case 41:
-                prizeSelector = 5;
-                break;
-            case 32:
-                prizeSelector = 6;
-                break;
-            case 40:
-                prizeSelector = 7;
-                break;
-            case 22:
-                prizeSelector = 8;
-                break;
-            case 31:
-                prizeSelector = 9;
-                break;
-            case 30:
-                prizeSelector = 10;
-                break;
-            case 12:
-                prizeSelector = 11;
-                break;
-            case 21:
-                prizeSelector = 12;
-                break;
-            case 20:
-                prizeSelector = 13;
-                break;
-            default:
-                prizeSelector = 0;
-                break;
+            }
         }
-        return prizeSelector;
+        return level;
     }
+    //Processes game leaving command
     public String exit(){
         String exitString = "";
         String formattedNumber = "";
-        Double[] percent = {43.2,4.15,1.92,1.45,1.48,1.67,1.38,1.75,2.85,4.1,4.95,13.85,17.25};
+        double[] percent = {43.2, 4.15, 1.92, 1.45, 1.48, 1.67, 1.38, 1.75, 2.85, 4.1, 4.95, 13.85, 17.25};
         // Replace percent array with each prizes value
-        for (int i = 0;percent.length>i;i++){
-            percent[i]=prizePool*percent[i]*0.01/winnerArray[i];
+        for (int i = 0; i < percent.length; i++){
+            percent[i] = prizePool * percent[i] * 0.01 / winnerArray[i];
         }
         //System.out.println(Arrays.toString(percent));
 
-        for (int i = 0;winnerArray.length>i;i++){
-            if(percent[i]!=1/0f){
-                formattedNumber = String.format("%.2f",percent[i]).replaceAll(",",".");
+        for (int i = 0; winnerArray.length > i; i++){
+            if(percent[i] != 1 / 0.f) {
+                formattedNumber = String.format("%.2f", percent[i]);
                 // Nivel: 1 Jogadores: 999 Valor premio: 999999,99 Euros
-                exitString += "Nivel: " + (i+1) + " Jogadores: " + winnerArray[i] + " Valor premio: " + formattedNumber+ " Euros\n";
-            }else{
-                exitString += "Nivel: " + (i+1) + " Jogadores: " + winnerArray[i] + "\n";
+                exitString += "Nivel: " + (i + 1) + " Jogadores: " + winnerArray[i] + " Valor premio: " + formattedNumber+ " Euros\n";
+            } else {
+                exitString += "Nivel: " + (i + 1) + " Jogadores: " + winnerArray[i] + "\n";
             }
             
         }
         // Change remaining cash money
-        for (int i=0; i<percent.length;i++){
-            if (percent[i]!=1/0f)
-            prizePool -= percent[i]*winnerArray[i];
+        for (int i = 0; i < percent.length; i++){
+            if (percent[i] != 1/0.)
+                prizePool -= percent[i] * winnerArray[i];
         }
-        if (prizePool<0){
-            prizePool = 0d;
+        if (prizePool < 0){
+            prizePool = 0.;
         }
 
-        formattedNumber = String.format("%.2f",prizePool).replaceAll(",",".");
-        exitString += "Valor acumulado: " + formattedNumber+" Euros\n";
-        //System.out.println("exitString: " + exitString);
+        formattedNumber = String.format("%.2f", prizePool);
+        exitString += "Valor acumulado: " + formattedNumber + " Euros\n";
         return exitString;
     }
+    // Processes main leave command
     public String bye(){
-        // Valor acumulado: 0.00 Euros. Ate a proxima.
         
-        return "Valor acumulado: "+String.format("%.2f",prizePool).replaceAll(",",".")+" Euros. Ate a proxima.";
+        return "Valor acumulado: " + String.format("%.2f",prizePool) + " Euros. Ate a proxima.";
     }
+
     // Game state operators
     public boolean getGameState(){
         return inGame;
     }
+    // Sets inGame to false
     public void falseGame(){
         inGame = false;
     }
+    // Returns main menu state
     public boolean getMenuState(){
         return inMenu;
     }
+    // Sets main menu state to false
     public void falseMenu(){
         inMenu = false;
     }
+    // Returns current prizepool
     public Double getprizepool(){
         return prizePool;
     }
--- Main.java
+++ Main.java
@@ -1,45 +1,63 @@
 /**@author Tiago Teles */
-/* Todas as funcoes com main no inicio do nome referem-se ao que acontece antes de haver um jogo iniciado,
-*  as com game no inicio serao entao as ja dentro do jogo.
+/* All functions in Main class that start with "main" have something to do with the main menu, also known as being outside a game, functions whose name starts with
+* game are used when inside a game..
 */
 import java.util.Scanner;
-class Main{
+
+class Main {
     private static final String SAIR = "sai";
+    private static final String SAIRHELP = " - Termina a execucao do programa";
+
     private static final String JOGAR = "joga";
+    private static final String JOGARHELP = " - Simula uma aposta, dando uma chave";
+
     private static final String AJUDA = "ajuda";
+    private static final String AJUDAHELP = " - Mostra os comandos existentes";
+
     private static final String NOVOJOGO = "novo";
+    private static final String NOVOJOGOHELP = " - Novo jogo dando um valor inicial";
+
     private static final String FIM = "fim";
+    private static final String FIMHELP = " - Termina o jogo em curso";
+
+    private static final String THANKSFORBET = "Obrigado pela aposta.";
+    private static final String INEXISTENTE = "Comando inexistente.";
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         FCTMil milObj = new FCTMil();
         mainCommands(scan,milObj);
     }
-    //Aqui escolhemos o que fazer com os comandos inseridos na consola quando fora do um jogo
+
+    //Here we chose what to do with the inserted commands when outside a game
     public static void mainCommands(Scanner scan,FCTMil milObj){
         while(milObj.getMenuState()){
             System.out.print("> ");
             String command = scan.next();
             switch(command.toLowerCase()){
-                case SAIR:mainLeave(scan, milObj); 
+                case SAIR:
+                	mainLeave(scan, milObj);
                     break;
-                case AJUDA:mainHelp(scan, milObj);
+                case AJUDA:
+                	mainHelp(scan, milObj);
                     break;
-                case NOVOJOGO:gameStart(scan, milObj);
+                case NOVOJOGO:
+                	gameStart(scan, milObj);
                     break;
                 default:
                     scan.nextLine();
-                    System.out.println("Comando inexistente.");
+                    System.out.println(INEXISTENTE);
                     break;
             }
             
         }
     }
-    //Aqui escolhemos o que fazer com os comandos inseridos na consola quando fora do um jogo
+
+    //Here we chose what to do with the inserted commands when inside a game
     public static void gameCommands(Scanner scan, FCTMil milObj){
         while(milObj.getGameState()){
             System.out.print("FCTMILHOES> ");
             String command = scan.next();
-            
             switch(command.toLowerCase()){
                 case FIM:
                     scan.nextLine();
@@ -55,51 +73,60 @@ class Main{
                     break;
                 default:
                     scan.nextLine();
-                    System.out.println("Comando inexistente.");
+                    System.out.println(INEXISTENTE);
                     break;
             }
-            
         }
     }
-    //Funcoes de interface
 
+    //Funcoes de interface
+    //Displays main menu help
     public static void mainHelp(Scanner scan, FCTMil milObj){
-        System.out.println(NOVOJOGO +" - Novo jogo dando um valor inicial\n"+ SAIR +" - Termina a execucao do programa\n"+ AJUDA +" - Mostra os comandos existentes");
+        System.out.println(NOVOJOGO + NOVOJOGOHELP);
+        System.out.println(SAIR + SAIRHELP);
+		System.out.println(AJUDA + AJUDAHELP);
     }
+    //Leaves the program
     public static void mainLeave(Scanner scan, FCTMil milObj){
         System.out.println(milObj.bye());
         milObj.falseMenu();
     }
+    //Starts game with a certain prizepool
+    public static void gameStart(Scanner scan, FCTMil milObj){
+        double prizePool = scan.nextDouble();
+        if (prizePool > 0) {
+        	milObj.newGame(prizePool);
+        	System.out.printf("Jogo iniciado. Valor do premio: %.2f Euros.\n", milObj.getprizepool());
+        } else {
+            System.out.println("Valor incorrecto.");
+		} 
+        gameCommands(scan,milObj);
+    }
+    //Ends the game and enters the main menu
     public static void gameEnd(Scanner scan, FCTMil milObj){
         milObj.falseGame();
         System.out.print(milObj.exit());
     }
-    public static void gameStart(Scanner scan, FCTMil milObj){
-        double prizePool = scan.nextDouble();
-        if (prizePool > 0){
-        milObj.newGame(prizePool);
-        System.out.printf("Jogo iniciado. Valor do premio: %.2f Euros.\n",milObj.getprizepool());
-            }else{
-                System.out.println("Valor incorrecto.");
-            } 
-        gameCommands(scan,milObj);
-        
-    }
+    
+    //Places a bet with a certain key
     public static void gamePlay(Scanner scan, FCTMil milObj){
         int[] bets = new int[7];
-        for(int i = 0;i<7;i++){
+        for(int i = 0; i<7; i++) {
             bets[i] = scan.nextInt();
         }
         int prizeLevel = milObj.bet(bets);
-        if (prizeLevel > 0){
-            System.out.println("Obrigado pela aposta. Premio de nivel: "+prizeLevel);
-        }else if(prizeLevel == -1){
+        if (prizeLevel > 0) {
+            System.out.println(THANKSFORBET +" Premio de nivel: "+prizeLevel);
+        } else if(prizeLevel == -1) {
             System.out.println("Chave incorrecta.");
-        }else{
-            System.out.println("Obrigado pela aposta.");
+        } else {
+            System.out.println(THANKSFORBET);
         }
     }
+    //Help command inside the game.
     public static void gameHelp(Scanner scan, FCTMil milObj){
-        System.out.println(JOGAR + " - Simula uma aposta, dando uma chave\n" + FIM + " - Termina o jogo em curso\n" + AJUDA + " - Mostra os comandos existentes");
+        System.out.println(JOGAR + JOGARHELP);
+        System.out.println(FIM + FIMHELP);
+        System.out.println(AJUDA + AJUDAHELP);
     }
 }
